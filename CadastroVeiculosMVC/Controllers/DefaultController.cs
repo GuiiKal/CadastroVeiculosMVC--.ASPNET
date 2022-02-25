f622603b74d23962ec831d3bf05ac5dfe41f2dca
Guilherme Kal <guiikal@hotmail.com>
2022-02-25 13:30:19 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroVeiculosMVC/Controllers/DefaultController.cs
@@ -0,0 +1,53 @@
+﻿using CadastroVeiculosMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadastroVeiculosMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            /*Veiculo v = new Veiculo();
+            v.Placa = "AA234G6"; //setando valores
+            string placaNova = v.Placa; //atribuindo a nova variavel*/
+
+            return View();
+        }
+        public ActionResult Listar()
+        {
+            List<Veiculo> lista = Veiculo.listarVeiculos();
+            return View(lista);
+        }
+
+        public ActionResult Excluir(string id)
+        {
+            Veiculo v = new Veiculo();
+            v.Placa = id;
+            string msg =  v.ExcluirM();
+            TempData["msg"] = msg; 
+            return RedirectToAction("Listar");
+        }
+
+        public ActionResult Salvar()
+        {
+            //esse método chama a view
+            return View();
+        }
+
+        [HttpPost] //esse metdo so trata requisições vindas dos formularios (method="post")
+        public ActionResult Salvar(string placa, string modelo)
+        {
+            //esse método recebe as informações
+            Veiculo v = new Veiculo();
+            v.Placa = placa;
+            v.Modelo = modelo;
+            TempData["msg"] = v.SalvarM();
+            return View();
+        }
+    }
+}
---
+++ CadastroVeiculosMVC/Models/Veiculo.cs
@@ -0,0 +1,103 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CadastroVeiculosMVC.Models
+{
+    public class Veiculo
+    {
+        const string conexaoBD = "Server=localhost; Database='AQUI VAI O NOME DO BD'; User id = root; Password='AQUI VAI A SENHA DO BD' ";
+        string modelo, placa, msg;
+        //criando propriedades
+        public string Placa
+        {
+            get { return placa; }
+            set { placa = value; }
+        }
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+
+        public  static List<Veiculo> listarVeiculos()
+        {
+            
+            List<Veiculo> lista = new List<Veiculo>();
+            MySqlConnection con = new MySqlConnection(conexaoBD);
+            try
+            {
+                con.Open();
+                MySqlCommand query = new MySqlCommand("select * from carro;", con);
+                MySqlDataReader leitor = query.ExecuteReader();
+                //percorre o resultado
+                while (leitor.Read())
+                {
+                    //cria o item da lista
+                    Veiculo item = new Veiculo();
+                    item.Placa = leitor["placa"].ToString();
+                    item.Modelo = leitor["modelo"].ToString();
+                    lista.Add(item);
+                }
+                
+            }
+            catch (Exception e2)
+            {
+                lista = null;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return lista;
+        }
+
+        public string ExcluirM()
+        {
+            MySqlConnection con = new MySqlConnection(conexaoBD);
+            try
+            {
+                con.Open();
+                MySqlCommand query= new MySqlCommand("delete from carro where placa = @placa;", con);
+                query.Parameters.AddWithValue("@placa", Placa);
+                query.ExecuteNonQuery();
+                msg = "Deletado com sucesso!";
+            }
+            catch (Exception ex1)
+            {
+                msg = "ERRO " + ex1.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return msg;
+        }
+
+        public string SalvarM()
+        {
+            MySqlConnection con = new MySqlConnection(conexaoBD);
+            try
+            {
+                con.Open();
+                MySqlCommand query = new MySqlCommand("insert into carro(placa, modelo) values(@placa, @modelo);", con);
+                query.Parameters.AddWithValue("@placa", Placa);
+                query.Parameters.AddWithValue("@modelo", Modelo);
+                query.ExecuteNonQuery();
+                msg = "Dados salvos com sucesso!";
+
+            }
+            catch (Exception ex2)
+            {
+                msg = "Falha ao salvar!" + ex2.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return msg;
+        }
+    }
+}
